e37abdfdefe0d2176afc6be6f118ffe19050acc3
fargabuanik <fargabanik@gmail.com>
2022-09-26 21:13:31 -0400

    updata

--- src/test/java/testPages/HomepagesTest.java
+++ src/test/java/testPages/HomepagesTest.java
@@ -3,26 +3,26 @@ package testPages;
 import org.testng.annotations.Test;
 import Base.BaseClass; 
 public class HomepagesTest extends BaseClass {
-	//@Test(enabled = true) 
+	@Test(enabled = true) 
 	public void logoTest() {
 		homePage.logoDisplayed();
 	}
-	//@Test(enabled = true)
+	@Test(enabled = true)
 	public void userIdFieldTest() {
 		homePage.enterUserIdField();
 	}
-	//@Test(enabled = true)
+	@Test(enabled = true)
 	public void loginButtonTest() throws InterruptedException {
 		homePage.loginButtonClicked();
 	}	
-//	@Test(enabled = true)
+	@Test(enabled = true)
 	public void insuranceButton() throws InterruptedException {
 		homePage.insuranceButton();
 	}
 	@Test(enabled = true)
 	public void healthButton() throws InterruptedException {
 		homePage.insuranceButton();
-		//homePage.healthButton();
+		homePage.healthButton();
 	}
 	
 }
